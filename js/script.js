a161e0d5487d9d92739c609fb412c15b1dd8563b
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-08-10 23:06:54 +0300

    seperates logic from the dom

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-use-before-define */
+/* eslint-disable no-undef */
 
 let booksArray = [];
 if (localStorage.myBooks != null) {
@@ -18,38 +19,6 @@ function myFunction() {
   return false;
 }
 
-function renderBook() {
-  const bookContainer = document.getElementById('booksContainer');
-  bookContainer.innerHTML = '';
-  const allBooks = JSON.parse(localStorage.myBooks);
-  for (let i = 0; i < allBooks.length; i += 1) {
-    const card = document.createElement('div');
-
-    const cardBody = document.createElement('div');
-
-    const title = document.createElement('h3');
-    title.innerText = allBooks[i].title;
-
-    const author = document.createElement('p');
-    author.innerText = allBooks[i].author;
-
-    cardBody.appendChild(title);
-    cardBody.appendChild(author);
-
-    card.appendChild(cardBody);
-
-    const deleteCard = document.createElement('button');
-    deleteCard.innerText = 'Delete';
-    deleteCard.addEventListener('click', (e) => {
-      e.preventDefault();
-      deleteBook(allBooks[i].title, allBooks[i].author);
-    });
-
-    cardBody.appendChild(deleteCard);
-
-    bookContainer.appendChild(card);
-  }
-}
 const updateStorage = () => {
   localStorage.myBooks = JSON.stringify(booksArray);
 };
@@ -72,5 +41,3 @@ const deleteBook = (title, name) => {
   updateStorage();
   renderBook();
 };
-
-renderBook();
