2ae49c2ad3ac7f6debee2ccad3ff65e0caba0893
Aristides Jose Molina Perez <aristides1000@gmail.com>
2021-08-11 17:52:20 -0400

    Add final styles and solved linter errors

--- js/dom.js
+++ js/dom.js
@@ -13,31 +13,28 @@ function renderBook() {
   bookContainer.innerHTML = '';
 
   for (let i = 0; i < allBooks.length; i += 1) {
-
-
     const cardBody = document.createElement('tr');
 
     const title = document.createElement('td');
-    title.classList.add('ms-4')
-    const spanelm = document.createElement('span')
-    spanelm.classList.add('ms-3')
-    spanelm.innerText = allBooks[i].title + "by " + allBooks[i].author;
-    title.appendChild(spanelm)
+    title.classList.add('ms-4', 'fs-5');
+    const spanelm = document.createElement('span');
+    spanelm.classList.add('ms-3');
+    spanelm.innerText = `"${allBooks[i].title}" by ${allBooks[i].author}`;
+    title.appendChild(spanelm);
 
     cardBody.appendChild(title);
-  
 
-    const deletebtn = document.createElement('td')
+    const deletebtn = document.createElement('td');
     deletebtn.classList.add('d-flex');
     const deleteCard = document.createElement('button');
-    deleteCard.classList.add('btn', 'btn-warning')
+    deleteCard.classList.add('btn', 'btn-warning', 'fs-5');
     deleteCard.innerText = 'Delete';
-    deleteCard.classList.add('mx-auto')
+    deleteCard.classList.add('mx-auto');
     deleteCard.addEventListener('click', (e) => {
       e.preventDefault();
       bookList.deleteBook(allBooks[i].title, allBooks[i].author);
     });
-    deletebtn.appendChild(deleteCard)
+    deletebtn.appendChild(deleteCard);
     cardBody.appendChild(deletebtn);
 
     bookContainer.appendChild(cardBody);
