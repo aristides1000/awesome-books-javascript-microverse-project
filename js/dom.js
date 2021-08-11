b97997eab0b102528785e1f385509041039fad91
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2021-08-11 20:26:29 +0300

    this updates the render book function

--- js/dom.js
+++ js/dom.js
@@ -1,41 +1,48 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-loop-func */
+let allBooks;
+if (localStorage.myBooks != null) {
+  allBooks = JSON.parse(localStorage.myBooks);
+} else {
+  allBooks = [];
+}
 
+const bookList = new BookList(allBooks);
 function renderBook() {
   const bookContainer = document.getElementById('booksContainer');
   bookContainer.innerHTML = '';
-  let allBooks;
-  if (localStorage.myBooks != null) {
-    allBooks = JSON.parse(localStorage.myBooks);
 
-    for (let i = 0; i < allBooks.length; i += 1) {
-      const card = document.createElement('div');
+  for (let i = 0; i < allBooks.length; i += 1) {
+    const card = document.createElement('div');
 
-      const cardBody = document.createElement('div');
+    const cardBody = document.createElement('div');
 
-      const title = document.createElement('h3');
-      title.innerText = allBooks[i].title;
+    const title = document.createElement('h3');
+    title.innerText = allBooks[i].title;
 
-      const author = document.createElement('p');
-      author.innerText = allBooks[i].author;
+    const author = document.createElement('p');
+    author.innerText = allBooks[i].author;
 
-      cardBody.appendChild(title);
-      cardBody.appendChild(author);
+    cardBody.appendChild(title);
+    cardBody.appendChild(author);
 
-      card.appendChild(cardBody);
+    card.appendChild(cardBody);
 
-      const deleteCard = document.createElement('button');
-      deleteCard.innerText = 'Delete';
-      deleteCard.addEventListener('click', (e) => {
-        e.preventDefault();
-        deleteBook(allBooks[i].title, allBooks[i].author);
-      });
+    const deleteCard = document.createElement('button');
+    deleteCard.innerText = 'Delete';
+    deleteCard.addEventListener('click', (e) => {
+      e.preventDefault();
+      bookList.deleteBook(allBooks[i].title, allBooks[i].author);
+    });
 
-      cardBody.appendChild(deleteCard);
+    cardBody.appendChild(deleteCard);
 
-      bookContainer.appendChild(card);
-    }
+    bookContainer.appendChild(card);
   }
 }
 
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  bookList.addBook();
+});
 renderBook();
